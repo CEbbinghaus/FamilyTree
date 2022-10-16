bb00c12bc7fe6e1db0ed108ffd2249154ca58be7
CEbbinghaus <git@cebbinghaus.com>
2022-10-16 17:44:27 +1100

    Added Grid background

--- src/components/SvgContainer.svelte
+++ src/components/SvgContainer.svelte
@@ -7,10 +7,26 @@
 	export const width = 1920;
 	export const height = 1080;
 
+	const gridSize = 20;
+
 	let svgElement: HTMLElement;
 	let gElement: HTMLElement;
+	let gridElement: HTMLElement;
 
 	onMount(() => {
+
+		// Call this function to modify the pattern when zooming/panning.
+		function updateGrid(zoomEvent: any) {
+			select(gridElement)
+				.attr('x', zoomEvent.transform.x)
+				.attr('y', zoomEvent.transform.y)
+				.attr('width', gridSize * zoomEvent.transform.k)
+				.attr('height', gridSize * zoomEvent.transform.k)
+				.selectAll('path')
+					.attr('x', gridSize * zoomEvent.transform.k / 2)
+					.attr('y', gridSize * zoomEvent.transform.k / 2)
+					.attr('opacity', Math.min(zoomEvent.transform.k, 1)); // Lower opacity as the pattern gets more dense.
+		}
 
 		const g = select(gElement);
 
@@ -19,6 +35,7 @@
 			.scaleExtent([.5, 5])
 			.on("zoom", function (evt) {
 				g.attr("transform", evt.transform);
+				updateGrid(evt);
 			}));
 
 		// make a svg
@@ -33,12 +50,19 @@
 			zoom.transform,
 			zoomIdentity.translate(width / 2, height / 2).scale(1)
 		).on("dblclick.zoom", null);
-
 	
 	})
 </script>
   
 <svg bind:this={svgElement}>
+	<defs>
+
+		<pattern bind:this={gridElement} id="grid" x=0 y=0 width="{gridSize}" height="{gridSize}" patternUnits="userSpaceOnUse">
+		  <path d="M {gridSize * 10} 0 L 0 0 0 {gridSize * 10}" fill="none" stroke="gray" stroke-width="1"/>
+		</pattern> 
+	</defs>
+		  
+	<rect width="100%" height="100%" fill="url(#grid)" />
 	<g bind:this={gElement} transform="translate({width / 2}, {height / 2})">
 		<SvgBox data={{x: 0, y: 0, width: 100, height: 100}}/>
 		<SvgBox data={{x: -300, y: -300, width: 100, height: 100}}/>
